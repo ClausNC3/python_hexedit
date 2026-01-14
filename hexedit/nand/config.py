541d90e14b92415a04856f94752f0479ab7fd870
Claus Christensen <git@karlslunde.nu>
2026-01-14 17:47:40 +0100

    Working on ECC

--- hexedit/nand/config.py
+++ hexedit/nand/config.py
@@ -14,44 +14,63 @@ class NANDFlashConfig:
     bbm_size: int
     padding_ranges: List[Tuple[int, int]]
     padding_size: int
+    page_ranges: List[Tuple[int, int]]
+    page_size: int
+    page_padding_ranges: List[Tuple[int, int]]
+    page_padding_size: int
+
     
-    # Eksempel med BBM midt i data
 NAND_CONFIGS = [
     NANDFlashConfig(
-        name="Test_BCH",
-        data_ranges=[(0x000, 0x12B), (0x12D, 0x200)],  # Data splittet pga BBM
-        data_size=0x200,
+        name="Test_BCH",        # K9F1208U0C (VW E-Call)
+        data_ranges=[(0x000, 0x12B), (0x12D, 0x200)],
+        data_size=512,
         ecc_ranges=[(0x201, 0x20D)],
         ecc_size=13,
         ecc_type=ECCType.BCH,
-        bbm_ranges=[(0x12C, 0x12C)],  # BBM byte midt i page
+        bbm_ranges=[(0x12C, 0x12C)],
         bbm_size=1,
-        padding_ranges=[(0x211, 0x21F)],
-        padding_size=2
+        padding_ranges=[(0x20E, 0x20F)],
+        padding_size=2,
+        
+        page_ranges=[(0x000, 0x20F)],
+        page_size=512 + 16,
+        page_padding_ranges=[],
+        page_padding_size=0
     ),
     NANDFlashConfig(
         name="Test_Hamming512",
         data_ranges=[(0x000, 0x1FF)],
-        data_size=0x200,
+        data_size=512,
         ecc_ranges=[(0x20D, 0x20F)],
         ecc_size=3,
         ecc_type=ECCType.HAMMING,
         bbm_ranges=[],
         bbm_size=0,
         padding_ranges=[(0x200, 0x20C)],
-        padding_size=13
+        padding_size=13,
+        
+        page_ranges=[(0x000, 0x20F)],
+        page_size=512 + 16,
+        page_padding_ranges=[],
+        page_padding_size=0
     ),
     NANDFlashConfig(
-        name="Test_Hamming256",
-        data_ranges=[(0x000, 0x1FF)],
-        data_size=0x200,
-        ecc_ranges=[(0x20A, 0x20F)],
+        name="Test_Hamming256",     # NAND128W3A2BN6 (Audi Telemodul)
+        data_ranges=[(0x000, 0x0FF), (0x100, 0x1FF)],
+        data_size=512,
+        ecc_ranges=[(0x20A, 0x20C), (0x20D, 0x20F)],
         ecc_size=6,
         ecc_type=ECCType.HAMMING,
         bbm_ranges=[],
         bbm_size=0,
         padding_ranges=[(0x200, 0x209)],
-        padding_size=10
+        padding_size=10,
+        
+        page_ranges=[(0x000, 0x20F)],
+        page_size=512+16,
+        page_padding_ranges=[],
+        page_padding_size=0
     ),
 ]
 
