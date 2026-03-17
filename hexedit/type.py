682728175511a72e4d2e6ff14008431ce20d4090
ClausNC3 <158768040+ClausNC3@users.noreply.github.com>
2026-03-17 10:43:33 +0100

    Add HAMMING512_NAND algo

---
+++ hexedit/type.py
@@ -0,0 +1,40 @@
+""" This file contains the different ECC types, that can be triggered by the NAND application.
+
+License:
+    MIT License
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+"""
+import enum
+
+class ECCType(enum.Enum):
+
+    # None
+    NONE                        = enum.auto()
+
+    # Hamming codes are a family of linear error-correcting codes
+    HAMMING256                  = enum.auto()  # Hamming for 256-byte blocks
+    HAMMING512                  = enum.auto()  # Hamming for 512-byte blocks (Standard: [LP0-3, LP4-7, CP+LP8])
+    HAMMING512_NAND             = enum.auto()  # Hamming for 512-byte blocks (NAND: [LP0+CP, LP1-4, LP5-8])    
+    HAMMING256_INVERTED         = enum.auto()  # Hamming for 256-byte blocks with inverted ECC
+    HAMMING512_INVERTED         = enum.auto()  # Hamming for 512-byte blocks with inverted ECC (Standard: [LP0-3, LP4-7, CP+LP8])
+    HAMMING512_NAND_INVERTED    = enum.auto()  # Hamming for 512-byte blocks with inverted ECC (NAND: [LP0+CP, LP1-4, LP5-8])    
+
+    # Bose–Chaudhuri–Hocquenghem codes (BCH codes) form a class of cyclic error-correcting codes that are constructed using polynomials over a finite field (also called a Galois field)
+    BCH                         = enum.auto()
